7e76c37dc50287f555942b71c7eddf681fac0ece
endlesslupita <endlesslupita@users.noreply.github.com>
2026-04-13 22:05:23 -0700

    Add incorrect increment twice to illustrate batching

--- portfolio-app/src/Counter.jsx
+++ portfolio-app/src/Counter.jsx
@@ -5,7 +5,23 @@ function Counter() {
 const [count, setCount] = useState(0);
 
 return ( 
-<div><button onClick={() => setCount(count + 1)}>Increment</button> <h1>count is {count}</h1><button onClick={() => setTimeout(() => setCount(count + 1), 2000)}>Increment after delay</button> <h1>count is {count}</h1></div>
+<div>
+
+<button onClick={() => setCount(count + 1)}>Increment</button>
+
+<h1>count is {count}</h1>
+
+<button onClick={() => setTimeout(() => setCount(count + 1), 2000)}>Increment after delay</button> 
+
+<h1>count is {count}</h1>
+
+<button onClick={() => {setCount(count + 1);
+setCount(count + 1);
+}}>Attempt increment twice</button>
+
+<h1>count is {count}</h1>
+
+</div>
 );
 }
 export default Counter;
